dfdf8cf8af9db9df96b9c920724b347b0f1cc899
Camila Kunitz <camila.kunitz@gmail.com>
2022-07-28 19:04:02 -0300

    Reajusta rota e estilo do Button

--- src/pages/Rotas.js
+++ src/pages/Rotas.js
@@ -1,16 +1,18 @@
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import Login from './Login';
+import VendaProduto from './NovaVenda/VendaProduto/VendaProduto';
 import Produtos from './Produtos/Produtos';
 import ProdutosForm from './Produtos/ProdutosForm';
 
-const Rotas = () => { 
+const Rotas = () => {
   return (
     <BrowserRouter>
-      <Routes>              
-        <Route path="/Login" exact element ={<Login />} />        
-        <Route path="/Produtos" exact element ={<Produtos />} />
-        <Route path="/EditarProduto" exact element ={<ProdutosForm />} />        
-      </Routes> 
+      <Routes>
+        <Route path="/Login" exact element={<Login />} />
+        <Route path="/Produtos" exact element={<Produtos />} />
+        <Route path="/EditarProduto" exact element={<ProdutosForm />} />
+        <Route path="/VendaProduto" exact element={<VendaProduto />} />
+      </Routes>
     </BrowserRouter>
   );
 };
