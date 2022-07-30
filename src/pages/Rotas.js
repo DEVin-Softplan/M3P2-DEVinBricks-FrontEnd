14a91bfe8de2324a942da83933ec928e10bcd1b5
elopes89 <manu021rj@gmail.com>
2022-07-29 22:46:14 -0300

    criando-modal

--- src/pages/Rotas.js
+++ src/pages/Rotas.js
@@ -5,17 +5,23 @@ import VendaResumo from './NovaVenda/VendaResumo';
 import Produtos from './Produtos/Produtos';
 import ProdutosForm from './Produtos/ProdutosForm';
 import Home from './Home/Home';
+import VendaEntrega from './Vendas/VendaEntrega';
 
 const Rotas = () => {
   return (
     <BrowserRouter>    
       <Routes>
-        <Route path="/" exact element ={<Home />} />        
+
+      <Route path="/" exact element ={<VendaEntrega />} />        
+
+        {/* <Route path="/" exact element ={<Home />} />  */}
         <Route path="/Login" exact element={<Login />} />
         <Route path="/Produtos" exact element={<Produtos />} />
         <Route path="/EditarProduto/:idProduto" exact element ={<ProdutosForm />} />        
         <Route path="/VendaProduto" exact element={<VendaProduto />} />
         <Route path="/VendaResumo" exact element={<VendaResumo />} />
+        {/* <Route path="/VendaEntrega" exact element={<VendaEntrega />} /> */}
+
       </Routes>
     </BrowserRouter>
   );
---
+++ src/pages/Vendas/VendaTabela.js
@@ -0,0 +1,26 @@
+import * as React from 'react';
+import { DataGrid } from '@mui/x-data-grid';
+
+const columns = [
+    { field: 'img', headerName: '#', width: 70, renderCell: 
+    (data) => <img src={data.row.img} width="40px" ></img>} ,
+    { field: 'produto', headerName: 'Produto', width: 130 },
+    { field: 'qtd', headerName: 'QTD', width: 130 },
+    { field: 'subTotal', headerName: 'Sub Total', width: 90, },
+];
+
+
+
+export default function VendaTabela({ carrinho }) {
+    return (
+        <div style={{ height: 220, width: 480 }}>
+            <DataGrid
+                rows={carrinho}
+                columns={columns}
+                pageSize={2}
+                // rowsPerPageOptions={[2]}
+
+            />
+        </div>
+    );
+}
