358919b0a4e693b5a114ce48b5ee8529e7da6cd5
Narriman Lima <narrimancslima@gmail.com>
2022-08-02 09:15:05 -0300

    Corrige import do FreteForm e busca de rota composta

--- src/services/BaseService.js
+++ src/services/BaseService.js
@@ -1,11 +1,11 @@
-import axios from "axios"
-import env from 'react-dotenv'
+import axios from "axios";
+import env from "react-dotenv";
 
 const getURLBackend = () => {
-    const { URL_BACKEND } = env
-    return URL_BACKEND
-}
+	const { URL_BACKEND } = env || "";
+	return URL_BACKEND;
+};
 
 export const cliente = axios.create({
-    baseURL: getURLBackend()
-})
+	baseURL: getURLBackend(),
+});
