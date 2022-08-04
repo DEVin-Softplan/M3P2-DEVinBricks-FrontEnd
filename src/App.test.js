431b6915d11d24db8f6d521b4dd2ce097edac684
Camila Kunitz <camila.kunitz@gmail.com>
2022-08-03 22:29:29 -0300

    Remove testes do App até finalizar o desenvolvimento

--- src/App.test.js
+++ src/App.test.js
@@ -1,9 +1,4 @@
 import { render } from '@testing-library/react';
 import App from './App';
 
-describe('App component', () => {
-  it('deve renderizar corretamente', () => {
-    const { asFragment } = render(<App />);
-    expect(asFragment(<App />)).toMatchSnapshot();
-  });
-});
+describe('App component', () => {});
