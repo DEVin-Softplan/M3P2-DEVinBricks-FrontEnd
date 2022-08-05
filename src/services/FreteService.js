fd5850684df36964e93e2f2a055d5add78dae6c6
Narriman Lima <narrimancslima@gmail.com>
2022-08-04 21:07:27 -0300

    Adiciona requisições da tela de Edição de Regra

--- src/services/FreteService.js
+++ src/services/FreteService.js
@@ -46,3 +46,52 @@ export const setNovaRegraDeFrete = async (param, token) => {
 		console.error(err);
 	}
 };
+
+export const atualizaRegraDeFrete = async (id, param, token) => {
+	try {
+		const response = await fetch(
+			`https://localhost:7171/api/FretePorEstado/editar/${id}`,
+			{
+				method: "PUT",
+				body: JSON.stringify(param),
+				headers: {
+					"Content-Type": "application/json; charset=utf-8",
+					Authorization: `bearer ${token}`,
+				},
+			}
+		);
+
+		if (response.status === 400) {
+			response.message = "Dados Inconsistentes, verifique o Id.";
+		}
+
+		return response;
+	} catch (err) {
+		console.error(err);
+	}
+};
+
+export const consultaFrete = async (token, nome) => {
+	try {
+		const response = await fetch(
+			`https://localhost:7171/api/FretePorEstado/?nome=${nome}`,
+			{
+				headers: {
+					Authorization: `bearer ${token}`,
+				},
+			}
+		);
+
+		if (!response.ok) {
+			throw new Error(
+				`This is an HTTP error: The status is ${response.status}`
+			);
+		}
+
+		let data = await response.json();
+
+		return data;
+	} catch (err) {
+		console.error(err);
+	}
+};
