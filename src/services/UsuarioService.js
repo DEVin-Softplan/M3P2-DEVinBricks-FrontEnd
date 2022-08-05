6855c4d4655b3bd2ae99941768d540aeb3875ba3
VERONI JOSE VIEIRA JUNIOR <veroni_vieira-jun1@estudante.sesisenai.org.br>
2022-08-05 14:20:07 -0300

    inserção e notificação na tela implementado

--- src/services/UsuarioService.js
+++ src/services/UsuarioService.js
@@ -1,43 +1,20 @@
 import axios from "axios";
-export const getUsuariosPost = async () => {
-  try {
-    const response = await fetch(`https://localhost:7171/api/Usuario`, {
-      headers: {
-        Authorization:
-          "bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJuYW1laWQiOiIxIiwibmFtZSI6IkFkbWluIiwiZW1haWwiOiJhZG1pbkBnbWFpbC5jb20iLCJpc19hZG1pbiI6IlRydWUiLCJuYmYiOjE2NTk2NTgzNjMsImV4cCI6MTY1OTY2NTU2MywiaWF0IjoxNjU5NjU4MzYzfQ.yfY1ufC5l76OMomoO218rZDCk_CaJxpFkCTV-3AVTOs",
-      },
-    });
-
-    if (!response.ok) {
-      throw new Error(
-        `This is an HTTP error: The status is ${response.status}`
-      );
-    }
-
-    let data = await response.json();
-
-    data.sort((a, b) => a.nome.localeCompare(b.nome));
-
-    return data;
-  } catch (err) {
-    console.error(err);
-  }
-};
 
 export const setNovoUsuario = async (usuario) => {
   try {
     // Envia uma requisição post
-    console.log(usuario)
-    const response = axios({
+    // console.log(usuario)
+    const response = await axios({
       method: "post",
       url: "https://localhost:7171/api/Usuario",
       headers: {
         Authorization:
-          "bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJuYW1laWQiOiIxIiwibmFtZSI6IkFkbWluIiwiZW1haWwiOiJhZG1pbkBnbWFpbC5jb20iLCJpc19hZG1pbiI6IlRydWUiLCJuYmYiOjE2NTk2NjE1NDEsImV4cCI6MTY1OTY2ODc0MSwiaWF0IjoxNjU5NjYxNTQxfQ.B3TWWG0_-7yY0Vp0yG2WadbOtTcLPT9TJwULoqyK5E0",
+          "bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJuYW1laWQiOiIxIiwibmFtZSI6IkFkbWluIiwiZW1haWwiOiJhZG1pbkBnbWFpbC5jb20iLCJpc19hZG1pbiI6IlRydWUiLCJuYmYiOjE2NTk3MTgxOTksImV4cCI6MTY1OTcyNTM5OSwiaWF0IjoxNjU5NzE4MTk5fQ.4AIWrBBi7lO0RhLFr0BOU02w3E8ey4JIGBVTTBW1Mt8",
+          // 'Content-Type': 'application/json'
       },
       data: usuario,
     });
-    
+
     if (response.status === 400) {
       response.message =
         "Já existe um cadastro de usuário. Tente novamente.";
@@ -45,6 +22,6 @@ export const setNovoUsuario = async (usuario) => {
     return response;
 
   } catch (err) {
-    console.error(err);
+    console.log("O erro esta aqui: ",err.response.data);
   }
 };
