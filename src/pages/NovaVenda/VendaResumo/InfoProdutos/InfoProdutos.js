b79e43df9421080134d10383d52f7f7fb331d96c
Vinícius <vinigsouza.bnu@gmail.com>
2022-08-06 10:17:49 -0300

    feat: implementa exclusão do produto do carrinho

--- src/pages/NovaVenda/VendaResumo/InfoProdutos/InfoProdutos.js
+++ src/pages/NovaVenda/VendaResumo/InfoProdutos/InfoProdutos.js
@@ -1,7 +1,10 @@
 import React, { useState, useContext } from 'react'
 import { VendaContext } from '../../../../contexts/VendaContext';
 import styles from './InfoProdutos.module.css'
-import { AiFillPlusCircle, AiFillMinusCircle } from 'react-icons/ai'
+import { AiOutlinePlusCircle, AiOutlineMinusCircle } from 'react-icons/ai'
+import { BiTrashAlt } from 'react-icons/bi'
+import { ToastContainer, toast } from 'react-toastify';
+import Swal from 'sweetalert2';
 
 const InfoProdutos = () => {
     const { dadosVenda, adicionarProdutos } = useContext(VendaContext);
@@ -15,6 +18,10 @@ const InfoProdutos = () => {
     const alterarQuantidade = (item, acrescentar) => {
         const index = produtos.findIndex(produto => produto.id === item.id)
         const handler = [...produtos]
+        if (handler[index].quantidade === 1 && !acrescentar) {
+            excluirProduto(item);
+            return
+        }
         handler[index].quantidade = acrescentar ? handler[index].quantidade + 1 : handler[index].quantidade - 1
         atualizarListaProdutos(handler)
     }
@@ -27,15 +34,38 @@ const InfoProdutos = () => {
         alterarQuantidade(item, false)
     }
 
+    const excluirProduto = (item) => {
+        Swal.fire({
+            title: 'Deseja mesmo excluir esse produto?',
+            text: "O produto será removido do carrinho após a exclusão!",
+            icon: 'warning',
+            showCancelButton: true,
+            cancelButtonText: 'Cancelar',
+            confirmButtonColor: '#5965E0',
+            cancelButtonColor: '#E83F5B',
+            confirmButtonText: 'Sim, remover!'
+          }).then((result) => {
+            if (result.isConfirmed) {
+                const index = produtos.findIndex(produto => produto.id === item.id)
+                const handler = [...produtos]
+                handler.splice(index, 1)
+                atualizarListaProdutos(handler)
+                toast(`${item.nome} foi removido do carrinho!`)
+            }
+          })
+    }
+
     return (
         <div className={styles.containerProdutos}>
+            <ToastContainer />
             <table className={styles.tabelaProdutos}>
                 <thead className={styles.tabelaHeader}>
                     <tr className={styles.tabelaHeader}>
                         <th style={{ width: '20%' }}>#</th>
-                        <th style={{ width: '40%' }}>Produto</th>
+                        <th style={{ width: '30%' }}>Produto</th>
                         <th style={{ width: '20%' }}>Quantidade</th>
                         <th style={{ width: '20%' }}>SubTotal</th>
+                        <th style={{ width: '10%' }}></th>
                     </tr>
                 </thead>
                 <tbody>
@@ -45,14 +75,15 @@ const InfoProdutos = () => {
                             <td className={styles.celulaProduto}><img width="90px" src={item.url} alt="Imagem do produto" /></td>
                             <td className={styles.celulaProduto}>{item.nome}</td>
                             <td className={styles.celulaProduto}>
-                                <AiFillMinusCircle className={styles.botaoQuantidade} onClick={() => diminuirQuantidade(item)} />
+                                <AiOutlineMinusCircle className={styles.botaoQuantidade} onClick={() => diminuirQuantidade(item)} />
                                 {item.quantidade}
-                                <AiFillPlusCircle className={styles.botaoQuantidade} onClick={() => adicionarQuantidade(item)} />
+                                <AiOutlinePlusCircle className={styles.botaoQuantidade} onClick={() => adicionarQuantidade(item)} />
                             </td>
                             <td className={styles.celulaProduto}>{(item.valor * item.quantidade).toLocaleString('pt-br', {
                                 style: 'currency',
                                 currency: 'BRL',
                             })}</td>
+                            <td className={styles.celulaProduto}><BiTrashAlt className={styles.botaoLixeira} onClick={() => excluirProduto(item)} /></td>
                         </tr>
                     ))}
                 </tbody>
